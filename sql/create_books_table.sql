35d4e607026657c154def89a31040777d59496be
IncompleteArchives <lena8787dog1@yandex.ru>
2026-08-01 23:49:20 +0300

    add author_id FK

--- sql/create_books_table.sql
+++ sql/create_books_table.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS public.books (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    author character varying(255) COLLATE pg_catalog."default",
     publication_year integer,
-    CONSTRAINT books_pkey PRIMARY KEY (id)
-    );
+    author_id integer,
+    CONSTRAINT books_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_books_author FOREIGN KEY (author_id)
+        REFERENCES public.authors (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
